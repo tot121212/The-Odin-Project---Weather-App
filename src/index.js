4b50c2a0084a4f2ae9e63c058d4707736a48af8d
tot121212 <totmusicbusiness@gmail.com>
2025-02-20 12:08:06 -0600

    added basic html layout, added event listeners to location form and toggle-temp-type button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!")
+// look at weather data and see what kind of elements we need to display...
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const locationElement = document.querySelector("location");
+    locationElement.addEventListener("change", ()=>{
+        // update weather info based upon that location
+    });
+    let curTempType = "F";
+    const toggleTempTypeButton = document.querySelector("toggle-temp-type");
+    toggleTempTypeButton.addEventListener("click", ()=>{
+        // update temp type displayed to either toggle
+        switch (curTempType){
+            case "F":
+                curTempType = "C";
+            case "C":
+                curTempType = "F";
+        }
+    });
+});
