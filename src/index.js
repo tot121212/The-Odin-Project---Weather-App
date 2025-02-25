4e75b3220794565fafca821d5c4c261e051313c7
tot121212 <totmusicbusiness@gmail.com>
2025-02-24 19:58:19 -0600

    added most of the code needed, just need to style it better...

--- src/index.js
+++ src/index.js
@@ -1,19 +1,98 @@
-// look at weather data and see what kind of elements we need to display...
+import "./css-reset.css";
+import "./template.css";
 
+import searchIcon from "./media/search.svg";
+import temperatureIcon from "./media/thermometer-half.svg";
+
+import {apiKey} from "./apiKey.js";
+
+const createObjectFromSVG = (svg)=>{
+    const object = document.createElement("object");
+    object.setAttribute("type", "image/svg+xml");
+    object.setAttribute("data", svg);
+    return object;
+}
+
+const addObjectToElement = (element, object)=>{
+    element.textContent = "";
+    element.innerHTML = "";
+    element.appendChild(object);
+}
+
+const degreeSymbol = "°";
+let unitGroup = "us";
+const getWeatherData = async (location) => {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&unitGroup=${unitGroup}`);
+    const weatherData = await response.json();
+    console.log("weatherData:",weatherData);
+    return { 
+        temp : weatherData.currentConditions.temp, 
+        description : weatherData.description,
+        feelslike : weatherData.currentConditions.feelslike
+    };
+}
+
+const formTempString = (temp) => {
+    return String(temp) + degreeSymbol + (unitGroup === "us" ? "F" : "C");
+}
+
+// add loading wheel
 document.addEventListener("DOMContentLoaded", ()=>{
-    const locationElement = document.querySelector("location");
-    locationElement.addEventListener("change", ()=>{
-        // update weather info based upon that location
-    });
-    let curTempType = "F";
-    const toggleTempTypeButton = document.querySelector("toggle-temp-type");
-    toggleTempTypeButton.addEventListener("click", ()=>{
-        // update temp type displayed to either toggle
-        switch (curTempType){
-            case "F":
-                curTempType = "C";
-            case "C":
-                curTempType = "F";
-        }
-    });
+    const locationElement = document.querySelector("input#location");
+    if (locationElement){
+        let curTimeout;
+        locationElement.addEventListener("change", ()=>{
+            // update weather info based upon that location
+            curTimeout && clearTimeout(curTimeout);
+            if (!locationElement.value) {return};
+            curTimeout = setTimeout(async ()=>{
+                const {temp, description, feelslike} = await getWeatherData(locationElement.value);
+
+                const stringTemp = formTempString(temp);
+                console.log("stringTemp:", stringTemp);
+                stringTemp & (document.querySelector(".current-temp").textContent = stringTemp);
+
+                document.querySelector(".description").textContent = description;
+
+                document.querySelector(".feels-like").textContent = "Feels like: " + formTempString(feelslike);
+            }, 100);
+        });
+    }
+    
+    const toggleTempTypeButton = document.querySelector("button#toggle-temp-type");
+    if (toggleTempTypeButton){
+        addObjectToElement(toggleTempTypeButton, createObjectFromSVG(temperatureIcon));
+        toggleTempTypeButton.addEventListener("click", async ()=>{
+            switch (unitGroup){
+                case "us":
+                    unitGroup = "metric";
+                    toggleTempTypeButton.classList.remove("fahrenheit");
+                    toggleTempTypeButton.classList.add("celsius");
+                    // search current query for celsius temp
+                    break;
+                case "metric":
+                    unitGroup = "us";
+                    toggleTempTypeButton.classList.remove("celsius");
+                    toggleTempTypeButton.classList.add("fahrenheit");
+                    // search current query for fahrenheit temp
+                    break;
+            }
+            console.log("unitGroup:",unitGroup);
+
+            locationElement.dispatchEvent(new Event("change"));
+
+            toggleTempTypeButton.classList.add("fade");
+            setTimeout(()=>{
+                toggleTempTypeButton.classList.remove("fade");
+            }, 300);
+        });
+    }
+
+    // const searchButton = document.querySelector("button#search");
+    // if (searchButton){
+    //     addObjectToElement(searchButton, createObjectFromSVG(searchIcon));
+    //     searchButton.addEventListener("click", ()=>{
+    //         // search
+    //     });
+    // }
 });
