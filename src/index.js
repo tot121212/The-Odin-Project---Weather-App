a68f0f508cd1e398b3ccd33fa4ceadac020b9b53
tot121212 <totmusicbusiness@gmail.com>
2025-02-25 15:47:10 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import "./template.css";
 
 import temperatureIcon from "./media/thermometer-half.svg";
 // import all svgs into one object for all weathers inside of media/4th-monochrome from visual crossing
+const importWeatherIcons = require.context("./media/4th-monochrome", false, /\.svg$/);
+const weatherIcons = {};
+importWeatherIcons.keys().forEach((fileName) => {
+    const iconName = fileName.replace('./', '').replace('.svg', ''); // Remove "./" and ".svg"
+    weatherIcons[iconName] = importWeatherIcons(fileName); // Import the SVG
+});
+
 
 import {apiKey} from "./apiKey.js";
 
@@ -25,7 +32,7 @@ const getWeatherData = async (location) => {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&unitGroup=${unitGroup}`);
         if (!response.ok){
-            throw new Error("Error fetching weather data");
+            throw new Error("Could not fetch weather data, invalid input?");
         }
         const weatherData = await response.json();
         console.log("weatherData:",weatherData);
@@ -33,7 +40,8 @@ const getWeatherData = async (location) => {
             locationName : weatherData.resolvedAddress,
             temp : weatherData.currentConditions.temp,
             description : weatherData.description,
-            feelslike : weatherData.currentConditions.feelslike
+            feelslike : weatherData.currentConditions.feelslike,
+            icon : weatherData.currentConditions.icon
         };
     } catch (error) {
         console.log(error);
@@ -41,7 +49,8 @@ const getWeatherData = async (location) => {
             locationName : "No Location Found",
             temp : "",
             description : "",
-            feelslike : ""  
+            feelslike : "",
+            icon : ""
         };
     }
 }
@@ -60,21 +69,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
             curTimeout && clearTimeout(curTimeout);
             if (!locationElement.value) {return};
             curTimeout = setTimeout(async ()=>{
-                const {locationName, temp, description, feelslike, iconName} = await getWeatherData(locationElement.value);
+                const {locationName, temp, description, feelslike, icon} = await getWeatherData(locationElement.value);
 
-                document.querySelector(".location-name").textContent = locationName;
+                const locationNameElement = document.querySelector(".location-name");
+                locationNameElement.textContent = locationName;
 
                 const stringTemp = formTempString(temp);
                 console.log("stringTemp:", stringTemp);
-                stringTemp & (document.querySelector(".current-temp").textContent = stringTemp);
 
-                document.querySelector(".description").textContent = description;
+                const currentTempElement = document.querySelector(".current-temp");
+                stringTemp && (currentTempElement.textContent = stringTemp);
 
-                document.querySelector(".feels-like").textContent = "Feels like: " + formTempString(feelslike);
+                const descriptionElement = document.querySelector(".description");
+                descriptionElement.textContent = description;
 
-                //set src
-                //document.querySelector(".weather-icon").src = "";
-            }, 600);
+                const feelsLikeElement = document.querySelector(".feels-like");
+                feelsLikeElement.textContent = "Feels like: " + formTempString(feelslike);
+
+                const iconPath = weatherIcons[icon];
+                console.log("iconPath:",iconPath);
+
+                const weatherIconElement = document.querySelector(".weather-icon");
+                weatherIconElement.src = iconPath;
+                weatherIconElement.alt = icon;
+
+                if (locationName === "No Location Found"){
+                    currentTempElement.hidden = true;
+                    descriptionElement.hidden = true;
+                    feelsLikeElement.hidden = true;
+                    weatherIconElement.hidden = true;
+                } else {
+                    currentTempElement.hidden = false;
+                    descriptionElement.hidden = false;
+                    feelsLikeElement.hidden = false;
+                    weatherIconElement.hidden = false;
+                }
+            }, 360);
         });
     }
     
@@ -106,9 +136,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }, 300);
         });
     }
-
-    const weatherIconElement = document.querySelector(".weather-icon");
-    weatherIconElement && addObjectToElement(weatherIconElement, createObjectFromSVG(weatherIcon));
     // const searchButton = document.querySelector("button#search");
     // if (searchButton){
     //     addObjectToElement(searchButton, createObjectFromSVG(searchIcon));
